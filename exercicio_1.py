4acd4cab03aaf1b401ed3f607ea445ed3b08f6b4
MateusRainha <mateusferreirarainha@enta.pt>
2022-10-03 09:02:52 +0000

    Exercícios para o teste

---
+++ exercicio_1.py
@@ -0,0 +1,15 @@
+"""     1. Escreva um programa em Python que pede ao utilizador que lhe forneça
+        dois números (x e y) e que escreve o valor de (x + 3 * y) * (x - y).
+        O seu programa deve gerar uma interação como a seguinte:
+        Vou pedir-lhe dois numeros
+        Escreva o primeiro numero, x = 5
+        Escreva o segundo numero, y = 6
+        O valor de (x + 3 * y) * (x - y) e: -23"""
+
+if __name__ == '__main__':
+    print()
+    print('VOU PEDIR-LHE DOIS NÚMEROS')
+x = int(input(f'Escreva o número x: '))
+y = int(input(f'Escreva o número y: '))
+conta = (x + 3 * y) * (x - y)
+print(f'O valor de (x + 3 * y) * (x - y) é: {conta}')
---
+++ exercicio_2.py
@@ -0,0 +1,6 @@
+"""     2. Escreva um programa em Python que lê valores correspondentes a uma
+        distância percorrida (em Km) e o tempo necessário para a percorrer (em
+        minutos), e calcula a velocidade média em:
+        (a) Km / h
+        (b) m/s             """
+
